3af7ed6470f2c5d80f4e24be97d66593a7b65549
Deb13579 <74122863+Deb13579@users.noreply.github.com>
2021-03-12 10:21:12 +0800

    Add files via upload

---
+++ Project #36/Js/fooooooooooooooooooooooood.js
@@ -0,0 +1,13 @@
+class Fooooooooooooooooooooooood {
+    constructor() {
+      
+    }
+  
+    display(){
+var title = createElement('h2')
+title.html("Feed the evil dog");
+title.position(130, 0);
+var food
+var time
+    }
+}
